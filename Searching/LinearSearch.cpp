a96bc2168e04cd017961064155a04788513c5780
Sushrut Kuchik <62986960+ksushrut@users.noreply.github.com>
2022-09-27 18:51:13 +0530

    Update LinearSearch.cpp

--- Searching/LinearSearch.cpp
+++ Searching/LinearSearch.cpp
@@ -1,1 +1,22 @@
-LINEAR SEARCH 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[4]={3,6,1,0};
+    int ele,i,len;
+    cout<<"Enter an element to be searched: ";
+    cin>>ele;
+    len=sizeof(arr)/sizeof(arr[0]);
+    for(i=0;i<len;i++)
+    {
+        if(arr[i]==ele)
+        {
+            cout<<"Element found at position "<<i+1;
+            break;
+        }
+
+    }
+    if(i==len)
+        cout<<"Element not found";
+    return 0;
+}
